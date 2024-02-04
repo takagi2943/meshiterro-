02ccbda824086b232843eeedf51b9f8c264feeb5
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-02-04 21:42:12 +0000

    登録機能の追加【controller,ストロングパラメータ,Routing】

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,12 +1,28 @@
 class PostImagesController < ApplicationController
   def new
     # Viewへ渡す為のインスタンス変数に空のModelオブジェクトを生成する
-    @post_image = Posut_Image.new
+    @post_image = PostImage.new
+  end
+
+  # 投稿データの保存
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user.id = current_user.id
+    @post_image.save
+    redirecto_to post_images_path
   end
 
   def index
   end
 
   def show
+  end
+
+
+  # 投稿データのストロングパラメータ
+  praivate
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 end
