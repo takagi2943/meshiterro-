479a28fc2ac324b0a18da6dd07e5dc9a45e1499f
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-02-05 09:13:21 +0000

    アプリ編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
-
+  root to: "homes#top"
+  get '/home/about' => 'homes#about', as: "about"
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
- get '/home/about' => 'homes#about', as: "about"
 end
