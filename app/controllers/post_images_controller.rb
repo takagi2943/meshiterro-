27783c2a74d9759076edaba1ed85dc0bcd1e2bd3
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-02-07 05:31:39 +0000

    スペルミス修正

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -18,7 +18,7 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
-    @post_comment = PsoComment.
+    @post_comment = PostComment
   end
 
   def destroy
