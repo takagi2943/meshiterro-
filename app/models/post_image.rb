0418469ac52ff25fa4139a88f3b7e22ff32d1746
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-02-07 09:01:04 +0000

    アプリ編集

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,9 +1,10 @@
 class PostImage < ApplicationRecord
+  belongs_to :user
   attribute :shop_name, :string
   attribute :caption, :text
   has_one_attached :image
-  belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   def get_image
    unless image.attached?
@@ -13,4 +14,7 @@ class PostImage < ApplicationRecord
    image
   end
 
+  def favorited_by?(user)
+      favorites.exists?(user_id: user.id)
+  end
 end
