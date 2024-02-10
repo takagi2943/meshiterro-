b88e9e0e096b36999a178d4997ede1115f76fbfe
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-02-10 08:33:12 +0000

    アプリ更新

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,10 +1,11 @@
 class PostImage < ApplicationRecord
-  belongs_to :user
-  attribute :shop_name, :string
-  attribute :caption, :text
   has_one_attached :image
+  belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+  validates :shop_name, presence: true
+  validates :image, presence: true
 
   def get_image
    unless image.attached?
